6a3fddebb6f98bb5bdf143c03d5f4b02da5c0322
Bobby Cheema <74267935+bobbyscheema@users.noreply.github.com>
2026-08-19 14:18:14 -0700

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pairs = {}
+
+        for i, value in enumerate(nums):
+            if target - value in pairs:
+                return [pairs[target - value], i]
+            pairs[value] = i
+    
