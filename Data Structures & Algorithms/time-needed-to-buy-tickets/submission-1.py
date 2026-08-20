63ebeb9cc7602c6e5c4fbd1167eb3d103055279c
Bobby Cheema <74267935+bobbyscheema@users.noreply.github.com>
2026-08-20 16:04:07 -0700

    Add: time-needed-to-buy-tickets - submission-1

---
+++ Data Structures & Algorithms/time-needed-to-buy-tickets/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        time = 0
+
+        for i in range(len(tickets)):
+            if tickets[i] <= tickets[k]:
+                time += tickets[i]
+            else:
+                time += tickets[k]
+        
+        return time
+
+         
+
+        
+            
