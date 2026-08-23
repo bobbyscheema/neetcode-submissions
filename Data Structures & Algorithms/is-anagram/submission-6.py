3db9ed6fa7468cee7c1258471cd0847149a7fdeb
Bobby Cheema <74267935+bobbyscheema@users.noreply.github.com>
2026-08-22 20:02:38 -0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        else:
+            match = {}
+            for i, char in enumerate(s):
+                match[char] = match.get(char, 0) + 1
+
+            for j, char in enumerate(t):
+                if char not in match or match[char] == 0:
+                    return False                
+        
+            return True
