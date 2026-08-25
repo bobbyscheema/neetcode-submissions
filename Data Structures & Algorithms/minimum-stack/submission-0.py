50530f70eb4609e929eb3c2c4ae99f83b86c1eda
Bobby Cheema <74267935+bobbyscheema@users.noreply.github.com>
2026-08-24 23:05:31 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.items = []
+        self.minStack = []
+        
+    def push(self, val: int) -> None:
+        self.items.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+        
+
+    def pop(self) -> None:
+        self.items.pop()
+        self.minStack.pop()
+        
+
+    def top(self) -> int:
+        return self.items[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+        
